020801b384ab7f09a1111c921f5c3074fdabc3be
phenomenelik <phenomenelik@outlook.com>
2021-09-24 18:49:10 +0200

    first test - open otomoto

---
+++ README.md
@@ -0,0 +1,10 @@
+otomoto-testing
+
+## Search your dream car on otomoto.pl
+
+otomoto tests: 🤖
+
+* supports: Java 11
+* supports: selenium>=3.141.59
+* docs: todo
+* sample project: https://github.com/phenomenelik
--- src/test/java/otomotoTest.java
+++ src/test/java/otomotoTest.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class otomotoTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.junit.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class otomotoTest {
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Test
+    public void openOtomoto() {
+        WebDriver driver = new ChromeDriver();
+        String title = "OTOMOTO - nowe i używane samochody i motocykle oraz części samochodowe. Ogłoszenia motoryzacyjne.";
+        driver.get("https://otomoto.pl");
+        assertEquals(title, driver.getTitle());
+        driver.quit();
+    }
 }
